2292fc7bef1fbc279dc8d07f029d68a949902c89
Dinesh-Beniwal-15 <dineshbeniwal1515@gmail.com>
2026-04-07 10:57:45 +0530

    Problem 5

--- problem3.java
+++ problem3.java
@@ -1,32 +1,22 @@
 import java.util.*;
 
-public class Problem4 {
-
-    static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
-
-    static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] > pivot) { // DESC
-                i++;
-                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
-            }
-        }
-        int temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
-        return i + 1;
-    }
+public class Problem5 {
 
     public static void main(String[] args) {
-        int[] arr = {12, 8, 15};
-        quickSort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
+
+        String[] arr = {"accA", "accB", "accB", "accC"};
+
+        // Linear search
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("accB")) {
+                System.out.println("Found at index " + i);
+                break;
+            }
+        }
+
+        // Binary search
+        Arrays.sort(arr);
+        int index = Arrays.binarySearch(arr, "accB");
+        System.out.println("Binary index: " + index);
     }
 }
