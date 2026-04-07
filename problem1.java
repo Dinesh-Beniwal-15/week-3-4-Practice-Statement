cbbecd92b1d511af84aecfccbd98ac6d4cd96d52
Dinesh-Beniwal-15 <dineshbeniwal1515@gmail.com>
2026-04-07 10:45:37 +0530

    Problem 2

--- problem1.java
+++ problem1.java
@@ -1,44 +1,37 @@
 import java.util.*;
 
-class Transaction {
-    String id;
-    double fee;
-    String time;
+class Client {
+    String name;
+    int risk;
 
-    Transaction(String id, double fee, String time) {
-        this.id = id;
-        this.fee = fee;
-        this.time = time;
-    }
-
-    public String toString() {
-        return id + ":" + fee;
+    Client(String n, int r) {
+        name = n;
+        risk = r;
     }
 }
 
-public class Problem1 {
+public class Problem2 {
     public static void main(String[] args) {
 
-        List<Transaction> list = new ArrayList<>();
-        list.add(new Transaction("id1", 10.5, "10:00"));
-        list.add(new Transaction("id2", 25.0, "09:30"));
-        list.add(new Transaction("id3", 5.0, "10:15"));
+        Client[] arr = {
+                new Client("A", 20),
+                new Client("B", 50),
+                new Client("C", 80)
+        };
 
-        // Bubble Sort (by fee)
-        for (int i = 0; i < list.size() - 1; i++) {
-            for (int j = 0; j < list.size() - i - 1; j++) {
-                if (list.get(j).fee > list.get(j + 1).fee) {
-                    Collections.swap(list, j, j + 1);
+        // Bubble sort ascending
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j].risk > arr[j + 1].risk) {
+                    Client temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
 
-        System.out.println("Bubble Sort: " + list);
-
-        // Outliers
-        System.out.println("High fee >50:");
-        for (Transaction t : list) {
-            if (t.fee > 50) System.out.println(t);
-        }
+        System.out.println("Sorted:");
+        for (Client c : arr)
+            System.out.println(c.name + ":" + c.risk);
     }
 }
